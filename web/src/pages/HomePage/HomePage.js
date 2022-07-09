965e8cec5002c169325dd877bedfcfb0e7cf4265
Ashutosh Modi <ashutosh94g@gmail.com>
2022-07-09 11:34:42 +0530

    Added post to prisma and generated Post

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      This is the main page
+    </>
+  )
+}
+
+export default HomePage
